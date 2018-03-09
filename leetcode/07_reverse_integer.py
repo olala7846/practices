514970518d6d2940b9d1d075163516977518dd46
Hsin-Cheng Chao <olala7846@gmail.com>
2018-03-09 18:25:26 +0800

    leetcode 08

--- leetcode/07_reverse_integer.py
+++ leetcode/07_reverse_integer.py
@@ -8,21 +8,24 @@ def reverse_integer(x):
         reverse += x % 10
         x = x // 10
 
-    x = -x if is_neg else x
-    if -(2**31) < x < (2*31 -1):
-        return x
+    reverse = -reverse if is_neg else reverse
+
+    if -(2**31) <= reverse <= (2**31 -1):
+        return reverse
     else:
         return 0
-
 
 assert reverse_integer(123) == 321
 assert reverse_integer(-123) == -321
 assert reverse_integer(7) == 7
 assert reverse_integer(100) == 1
 assert reverse_integer(-100) == -1
-assert reverse_integer(2**32) == 0
-assert reverse_integer(2**32-1) != 0
-assert reverse_integer(-2**31 -1) == 0
-assert reverse_integer(-2**31) != 0
+assert reverse_integer(2**31) == 0
+
+num_rev = int(str(2**31)[::-1])
+assert reverse_integer(num_rev) == 0
+
+num_rev = int(str(2**31 - 1)[::-1])
+assert reverse_integer(num_rev) != 0
 
 print('All test cases passed')
---
+++ leetcode/08_atoi.py
@@ -0,0 +1,26 @@
+
+def atoi(s):
+    s = s.strip()
+    idx = 0
+    is_neg = False
+    if s[0] == '-':
+        is_neg = True
+        idx += 1
+    elif s[0] == '+':
+        idx += 1
+
+    num = 0
+    while idx < len(s):
+        num *= 10
+        num += (ord(s[idx]) - ord('0'))
+        idx += 1
+
+    return num if not is_neg else -num
+
+assert atoi('123') == 123
+assert atoi('-123') == -123
+assert atoi('0') == 0
+assert atoi('   123') == 123
+assert atoi('\t123') == 123
+assert atoi('+123') == 123
+print('All test cases passed')
